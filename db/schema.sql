009cb8c5fc2df35bc6245d563e11f2e8a878cff2
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-01-30 23:38:09 -0800

    Completed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -14,13 +14,11 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-
     PRIMARY KEY (id),
 
     FOREIGN KEY (department_id)
-    REFERENCES department (id)
-    ON DELETE SET NULL
-
+        REFERENCES department (id)
+            ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -29,18 +27,19 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT NOT NULL,
     manager_id INT,
+    PRIMARY KEY (id, role_id, manager_id),
 
-    PRIMARY KEY (id, role_id, manager_id)
-
-    -- CONSTRAINT fk_employeeRole
-        FOREIGN KEY (role_id)
-        REFERENCES role (id)
+-- CONSTRAINT fk_employeeRole
+    FOREIGN KEY (role_id) 
+        REFERENCES role(id)
             ON UPDATE CASCADE,
 
-    -- CONSTRAINT fk_employeeManager
-        FOREIGN KEY (manager_id)
-        REFERENCES employee (id)
+-- CONSTRAINT fk_employeeManager
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+            ON UPDATE CASCADE
             ON DELETE CASCADE
-
+    
 );
 
+ 
