06ff1bd8a08df3e8ebcc6f8d07c218cb61dd1473
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-02-01 00:13:10 -0800

    Generated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,10 +15,11 @@ VALUES (1, "UX Designer", 130000.00, 2),
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "Mark", "Thompson", 2, NULL),
-       (2, "Stephanie", "Martinez", 4, 4),
-       (3, "Evori", "Dawson", 3, 4),
-       (4, "Cameron", "Sanchez", 1, 2),
-       (5, "Richard", "Smith", 6, 1),
-       (6, "Leslie", "Williams", 5, NULL)
+       (2, "Leslie", "Williams", 5, NULL),
+       (3, "Stephanie", "Martinez", 4, 1),
+       (4, "Evori", "Dawson", 3, 2),
+       (5, "Cameron", "Sanchez", 1, 3),
+       (6, "Richard", "Smith", 6, 1);
+
        
 
